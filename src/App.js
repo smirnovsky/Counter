7d5119abc8448639d6d7f1961361fde970ef9f1f
smirnovsky <happy280190@gmail.com>
2023-01-05 13:24:25 +0300

    add: add hook on react and finish project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React, { useState } from 'react';
 import './index.scss';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const counterPlus = () => {
+    setCounter(counter + 1)
+  }
+
+  const counterMinus = () => {
+    setCounter(counter - 1)
+  }
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{counter}</h1>
+        <button onClick={counterMinus} className="minus">-</button>
+        <button onClick={counterPlus} className="plus">+</button>
         <h1>Hello World</h1>
       </div>
     </div>
